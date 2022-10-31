55c3bb9218221b5870bde362d2303b4b53e055d0
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-31 11:40:15 +0100

    Create ComputerGuesserRandom.java

---
+++ GuessANumber/src/ComputerGuesserRandom.java
@@ -0,0 +1,27 @@
+public class ComputerGuesserRandom extends AComputerPlayer {
+    
+    private int min;
+    private int max;
+
+    public String getName() {
+        return "Computer Guesser";
+    }
+
+    public void newGame(int min, int max, String opponent, String type) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public int makeGuess() {
+        return (min + max) / 2;
+    }
+
+    public void guessFeedback(Answer answer) {
+        switch (answer) {
+            case TOO_LOW -> min = ((min + max) / 2) + 1;
+            case TOO_HIGH -> max = ((min + max) / 2) - 1;
+            case CORRECT -> { /* do nothing */ }
+            default -> throw new RuntimeException("This should never happen");
+        }
+    }
+}
