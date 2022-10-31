c98e84f1d9d696129e81518288952683a2139fbd
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-31 11:41:58 +0100

    random guess within range

--- GuessANumber/src/ComputerGuesserRandom.java
+++ GuessANumber/src/ComputerGuesserRandom.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+
 public class ComputerGuesserRandom extends AComputerPlayer {
-    
+
+    private final Random random = new Random();
     private int min;
     private int max;
+    private int guess;
 
     public String getName() {
         return "Computer Guesser";
@@ -13,13 +17,14 @@ public class ComputerGuesserRandom extends AComputerPlayer {
     }
 
     public int makeGuess() {
-        return (min + max) / 2;
+        guess = random.nextInt(max - min + 1) + min;
+        return guess;
     }
 
     public void guessFeedback(Answer answer) {
         switch (answer) {
-            case TOO_LOW -> min = ((min + max) / 2) + 1;
-            case TOO_HIGH -> max = ((min + max) / 2) - 1;
+            case TOO_LOW -> min = guess + 1;
+            case TOO_HIGH -> max = guess - 1;
             case CORRECT -> { /* do nothing */ }
             default -> throw new RuntimeException("This should never happen");
         }
