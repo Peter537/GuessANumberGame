d10f35bad81182740f00c1d8390b0efe67900e5b
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-11-01 11:59:52 +0100

    github test

--- GuessANumber/src/MainFeature.java
+++ GuessANumber/src/MainFeature.java
@@ -1,5 +1,9 @@
 public class MainFeature {
 
+    /*
+     * This class is just a Test for GitHub Merge Conflict
+     */
+
     public MainFeature() {
         NewFeature newFeature = new NewFeature();
         System.out.println(newFeature);
--- GuessANumber/src/NewFeature.java
+++ GuessANumber/src/NewFeature.java
@@ -1,5 +1,9 @@
 public class NewFeature {
-    
+
+    /*
+     * This class is just a Test for GitHub Merge Conflict
+     */
+
     @Override
     public String toString() {
         return "this is the new feature";
